cb07eb58a0e804462394656f2b77b33293eba54f
Ruben Ramirez <rframirez1993@gmail.com>
2023-06-18 15:23:15 -0700

    navigation bar. 404 page. register started. login placeholder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./pages/home/home.component";
+import {NotFoundComponent} from "./pages/not-found/not-found.component";
+import {RegisterComponent} from "./pages/register/register.component";
 
-const routes: Routes = [];
+// add components to map here
+const routes: Routes = [
+    { path: '', component: HomeComponent},
+    { path: 'register', component: RegisterComponent},
+    {path: '**', component:NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    NotFoundComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
